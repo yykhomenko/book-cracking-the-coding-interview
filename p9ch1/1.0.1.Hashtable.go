f02e308ff766d54b3f26a06cab31ae73d930372a
yykhomenko <yykhomenko@gmail.com>
2020-08-21 13:51:39 +0300

    rename

---
+++ p9ch1/1.0.1.Hashtable.go
@@ -0,0 +1,34 @@
+package p9ch1
+
+import (
+	"strconv"
+)
+
+type Hashtable struct {
+	array []int
+}
+
+// New returns initialized Hashtable
+func New() *Hashtable {
+	h := new(Hashtable)
+	h.array = make([]int, 10, 10)
+	return h
+}
+
+// Add the pair
+func (h *Hashtable) Add(k string, v int) {
+	index := hash(k) % len(h.array)
+	h.array[index] = v
+}
+
+func hash(v string) int {
+	return len(v)
+}
+
+func (h Hashtable) String() string {
+	s := ""
+	for k, v := range h.array {
+		s += strconv.Itoa(k) + " " + strconv.Itoa(v) + "\n"
+	}
+	return s
+}
