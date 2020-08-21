fc9f1237e44db1d145b04a0c3226d7f7847ed28e
yykhomenko <yykhomenko@gmail.com>
2020-08-21 13:57:48 +0300

    rename

--- p9ch1/1.0.1.Hashtable_test.go
+++ p9ch1/1.0.1.Hashtable_test.go
@@ -3,10 +3,12 @@ package p9ch1
 import (
 	"fmt"
 	"testing"
+
+	"github.com/yykhomenko/book-cracking-the-coding-interview/p9ch1/arraylist"
 )
 
 func TestHashtable_Add(t *testing.T) {
-	h := New()
+	h := arraylist.New()
 	h.Add("one", 1)
 	h.Add("five", 5)
 	fmt.Println(h)
