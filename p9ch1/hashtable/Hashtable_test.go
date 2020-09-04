9c106d66070e49b0aaa40a76b1348f13d384742f
yykhomenko <yykhomenko@gmail.com>
2020-09-04 18:55:38 +0300

    refactor Hashtable test

--- p9ch1/hashtable/Hashtable_test.go
+++ p9ch1/hashtable/Hashtable_test.go
@@ -1,14 +1,12 @@
-package p9ch1
+package hashtable
 
 import (
 	"fmt"
 	"testing"
-
-	"github.com/yykhomenko/book-cracking-the-coding-interview/p9ch1/arraylist"
 )
 
 func TestHashtable_Add(t *testing.T) {
-	h := arraylist.New()
+	h := New()
 	h.Add("one", 1)
 	h.Add("five", 5)
 	fmt.Println(h)
